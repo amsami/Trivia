1710d718f500ec5e0356d756854e5c45ddd271e4
Ahmed Mohamed Sami <moh.samiahmed@gmail.com>
2020-04-12 19:45:25 -0500

    With comments

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,7 +8,7 @@ import json
 from models import db, setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
-
+#Pagination function as it will be used a lot at different end points
 def paginate_questions(request, all_questions):
   page = request.args.get('page', 1, type=int)
   start = (page - 1) * QUESTIONS_PER_PAGE
@@ -23,12 +23,10 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
   '''
   @AK TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
   cors = CORS(app, resource={r"/api/*": {"origins": "*"}})
-
   '''
   @AK TODO: Use the after_request decorator to set Access-Control-Allow
   '''
@@ -37,13 +35,12 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,PUT,DELETE,OPTIONS')
         return response
-
-
   '''
   @AK TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  #Categories endpoint, where we load all categories and I have to change the format to meet the Json format
   @app.route('/categories')
   def all_categories():
    category_all = Category.query.all()
@@ -54,6 +51,7 @@ def create_app(test_config=None):
    if len(categories) == 0:
       abort(404)
 
+  #Change the format to dictionary to pass Json format
    formatted_categories = {k:v for category in categories for k,v in category.items()}
    return jsonify({
           'success': True,
@@ -70,6 +68,7 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  #Questions end point, passing all questions and all categories
   @app.route('/questions')
   def get_questions():
     questions = Question.query.all()
@@ -88,7 +87,6 @@ def create_app(test_config=None):
           'category': "",
           'categories': formatted_categories
     })
-
   '''
   @AK TODO: 
   Create an endpoint to DELETE question using a question ID. 
@@ -96,26 +94,27 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+  #Delete end point
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
     question = Question.query.filter(Question.id == question_id).one_or_none()
     if question is None:
       abort(404)
-
+    #based on the question id, delete the question, and update the db
     try:
       Question.query.filter(Question.id == question_id).delete()
       questions = Question.query.all()
-
+      #return new questions after deleting the question
       formatted_questions = paginate_questions(request, questions)
       if len(formatted_questions) == 0:
         abort(404)
-     
+      
       category_all = Category.query.all()
       categories = [category.format() for category in category_all]
 
       if len(categories) == 0:
         abort(404)
-
+       
       formatted_categories = {k:v for category in categories for k,v in category.items()}
       
       db.session.commit()
@@ -145,6 +144,7 @@ def create_app(test_config=None):
   @app.route('/questions', methods=['POST'])
   def add_question():
     try:
+      #get the new question data from json
       question = Question(
                           question = request.get_json()['question'],
                           answer = request.get_json()['answer'],
@@ -152,6 +152,7 @@ def create_app(test_config=None):
                           difficulty = request.get_json()['difficulty']
                           )
       question.insert()
+      #add question and commit to db
       db.session.commit()
 
       return jsonify({
@@ -174,6 +175,8 @@ def create_app(test_config=None):
   '''
   @app.route('/questions/search', methods=['POST'])
   def search_question():
+    #get Search term from Json, and then search based on the search term
+    #Return data to the format to json
     searchTerm = request.get_json()['searchTerm']
     data_searched=Question.query.filter(Question.question.ilike('%{}%'.format(searchTerm))).all()
     formatted_questions = paginate_questions(request, data_searched)   
@@ -196,7 +199,7 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that 
   category to be shown. 
   '''
-
+  #Get the set of questions based on a category
   @app.route('/categories/<int:category_id>/questions')
   def get_specific_question(category_id):
     questions_per_category = Question.query.filter(Question.category == category_id).all()
@@ -210,7 +213,6 @@ def create_app(test_config=None):
     formatted_categories = {k:v for category in categories for k,v in category.items()} 
 
     print(category_id, questions_per_category, formatted_questions, len(questions_per_category))
-
 
     try:
 
@@ -234,7 +236,12 @@ def create_app(test_config=None):
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
-  '''
+  '''  
+  #Quiz end point, get data from Json about category, and previous questions
+  # If category all is selected, category is set to 0, and check if it's the first question, then pass all questions
+  # If there was a previous question, then pass all questions not part of the previous questions set
+  # Otherwise, if thre is a category id, get questions from that category, and check for previous questions like the case 
+  # of all categories 
   @app.route('/quizzes', methods=['POST'])
   def quiz():
     response_quiz = request.get_json()
@@ -252,6 +259,7 @@ def create_app(test_config=None):
       else:
         questions = Question.query.filter(Question.id.notin_(previous_questions),
         Question.category == category_id).all()
+    #I got an error, because random.choice function would error out if there was an empty array
     #Needed to add a check here to avoid getting an error with the random.choice function.
     if len(questions) == 0:
         return jsonify({'question': None})
@@ -271,6 +279,7 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  #Setting up error handlers for all the different error messages
   @app.errorhandler(400)
   def bad_request(error):
       return jsonify({
